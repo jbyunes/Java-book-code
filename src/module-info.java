dd4cc789490d0ac1aa1bafbdf0a27bda98b39c84
Jean-Baptiste Yunès <Jean.Baptiste.Yunes@free.fr>
2026-03-26 14:28:50 +0100

    Commit testing.

--- src/module-info.java
+++ src/module-info.java
@@ -7,5 +7,6 @@ module fr.yunes.book {
     exports chapter.generic.intersection;
     exports chapter.threads.threads;
     exports chapter.threads.asynchronous;
+    
     requires java.desktop;
 }
